3b072701b89ad71c24d7514ba74457cbdca9c744
oqueber <oqueber@gmail.com>
2021-03-31 00:42:56 +0200

    actualizacion al español

--- web_app/src/index.js
+++ web_app/src/index.js
@@ -8,8 +8,8 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const methodOverride= require('method-override');
 const session = require('express-session');
-//const ip = "192.168.2.132";
-const ip = "127.0.0.1";
+const ip = "35.223.193.24";
+//const ip = "127.0.0.1";
 //const ip = '0.0.0.0';
 
 //Initializations
